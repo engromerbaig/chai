c643a2c50f187374f185d6a244715c72ab03f0f3
Omer <engromerbaig@gmail.com>
2024-08-16 04:41:07 +0200

    MORE DB CONNECTION

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = 'Chai';
+export const DB_NAME = 'Chai DB';
--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const app = express();
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        console.log('Database connected successfully');
+        console.log('MongoDB Database connected successfully');
     } catch (error) {
         console.error("Database connection error: ", error);
         process.exit(1);  // Exit the process with a failure code
